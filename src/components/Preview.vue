11aeb3ee1a3645ab7d7030ccfcd15bd356353a58
Warren Galyen <wgalyen@gmail.com>
2020-05-26 13:34:21 -0700

    chore: add/delete promote siganture

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -6,87 +6,166 @@
           <el-col :span="12">
             <h2>Signature preview</h2>
           </el-col>
-          <el-col :span="12" style="text-align: right;">
-            <el-select placeholder="Select template" v-model="selectTemplate">
+          <el-col
+            :span="12"
+            style="text-align: right;"
+          >
+            <el-select
+              v-model="selectTemplate"
+              placeholder="Select template"
+            >
               <el-option
                 v-for="item in template.list"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
-              ></el-option>
-              <el-option value="-1" disabled>Soon more templates</el-option>
+              />
+              <el-option
+                value="-1"
+                disabled
+              >Soon more templates</el-option>
             </el-select>
           </el-col>
         </el-row>
       </div>
       <div class="email">
         <div class="email-content">
-          <div class="line short"></div>
-          <br>
-          <div class="line long full"></div>
-          <div class="line long"></div>
-          <div class="line long full"></div>
-          <div class="line long full"></div>
-          <div class="line long"></div>
+          <div
+            class="line short"
+            style="margin-bottom: 30px;"
+          />
+          <div class="flex">
+            <div class="column-1">
+              <div class="line long full" />
+              <div class="line long full" />
+              <div class="line long full" />
+              <div class="line long full" />
+              <div class="line long full" />
+              <div class="line long full" />
+            </div>
+            <div class="column-2">
+              <carbon-ad v-if="isProd" />
+            </div>
+          </div>
+          <div class="line long" />
+          <div class="line long full" />
+          <div class="line long full" />
+          <div class="line long" />
+          <div class="line long full" />
         </div>
-        <div class="email-preview" ref="preview">
-          <component ref="template" :is="selectTemplate"></component>
+        <div
+          ref="preview"
+          class="email-preview"
+        >
+          <component
+            :is="selectTemplate"
+            ref="template"
+          />
         </div>
       </div>
       <div class="actions">
         <el-button-group>
-          <el-button size="small" type @click="viewSource">View source</el-button>
-          <el-button ref="copyHTML" size="small" @click="copySelect">Copy as Select</el-button>
-          <el-button size="small" type="primary" @click="copyHTML">Copy as HTML</el-button>
+          <el-button
+            size="small"
+            type
+            @click="viewSource"
+          >View source</el-button>
+          <el-button
+            size="small"
+            @click="copySelect"
+          >Copy as Select</el-button>
+          <el-button
+            ref="copyHTML"
+            size="small"
+            type="primary"
+            @click="copyHTML"
+          >Copy as HTML</el-button>
         </el-button-group>
         <br>
         <el-button
-          size="small"
           v-if="!showSetup"
-          type="text"
-          @click="showSetup = !showSetup"
-        >Show setup instruction
-        </el-button>
-        <el-button
           size="small"
-          v-if="showSetup"
           type="text"
           @click="showSetup = !showSetup"
-        >Hide setup instruction
-        </el-button>
+        >Show setup instruction</el-button>
+        <el-button
+          v-if="showSetup"
+          size="small"
+          type="text"
+          @click="showSetup = !showSetup"
+        >Hide setup instruction</el-button>
       </div>
-      <div class="setup-instruction" v-if="showSetup">
+      <div
+        v-if="showSetup"
+        class="setup-instruction"
+      >
         <h3>Basic usage:</h3>
         <p>Click on "Copy as HTML" button and paste snippet of your signature into your email client settings.</p>
         <h3>Advance usage:</h3>
-        <p>For some email clients, like gmail, you may using simply copy/paste highlight selection. Click on "Copy as
-          Select" button and paste of your signature into your email client settings.</p>
+        <p>For some email clients, like gmail, you may using simply copy/paste highlight selection. Click on "Copy as Select" button and paste of your signature into your email client settings.</p>
       </div>
-      <textarea ref="html" style="opacity: 0"></textarea>
+      <textarea
+        ref="html"
+        style="opacity: 0"
+      />
     </div>
-    <el-dialog title="Your email signature" :visible.sync="showSource">
+    <el-dialog
+      title="Your email signature"
+      :visible.sync="showSource"
+    >
       <p>Copy the HTML code below and paste it to your signature file</p>
       <br>
-      <el-input type="textarea" v-model="html" :rows="10" ref="dialogSource"></el-input>
-      <div style="text-align: right;" class="dialog-actions">
-        <el-button type="primary" @click="copySource">Copy</el-button>
+      <el-input
+        ref="dialogSource"
+        v-model="html"
+        type="textarea"
+        :rows="10"
+      />
+      <div
+        style="text-align: right;"
+        class="dialog-actions"
+      >
+        <el-button
+          type="primary"
+          @click="copySource"
+        >Copy</el-button>
       </div>
     </el-dialog>
-    <div class="preview__footer" v-if="showSignature">
-      <div class="donate" @click="onDonate">
-        <a
-          href="https://paypal.me/warrengalyen?locale.x=en_US"
-          target="_blank"
-          class="btn-donate paypal"
-        >buy me a coffee
-          <coffee-icon></coffee-icon>
-        </a>
-      </div>
-      <div class="copyright">
-        Development with
-        <heart-icon/>
-        by
-        <a href="https://www.mechanikadesign.com">Warren Galyen</a>
+    <div
+      v-if="true"
+      class="preview__footer"
+    >
+      <div class="support-project">
+        <el-popover
+          v-model="showDonatePopup"
+          placement="top"
+          width="260"
+        >
+          <p>Sigmark is a free project that developed by myself without funding.</p>
+          <p>If you liked the project, you may donate to support development and help it grow 🌱</p>
+          <ul>
+            <li><a
+              href="https://www.paypal.me/warrengalyen"
+              target="_blank"
+            >PayPal</a></li>
+          </ul>
+          <p>Or just share the project with your friends.</p>
+          <p>Are You a blogger? Please spread word about the project to subscribers.</p>
+          <p>You can <span
+            :class="{'link': !app.promoteSignature}"
+            @click="togglePromo(true)"
+          >leave</span> a promotional message in the signature, or <span
+            :class="{'link': app.promoteSignature}"
+            @click="togglePromo(false)"
+          >delete</span> it, but it will be sad 😭</p>
+          <div
+            slot="reference"
+            class="support-shield"
+            @click="onClickHelp"
+          >
+            Help project grow
+          </div>
+        </el-popover>
       </div>
       <div class="version">
         <span>current version:&nbsp;{{ version }}</span>
@@ -96,28 +175,25 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
-  import EmailTemplate1 from './templates/EmailTemplate1';
-  import HeartIcon from '../assets/image/heart.svg';
-  import CoffeeIcon from '../assets/image/coffee-cup.svg';
-
+  import { mapGetters, mapState } from 'vuex'
+  import EmailTemplate1 from './templates/EmailTemplate1'
   export default {
     name: '',
     components: {
       // eslint-disable-next-line
       EmailTemplate1,
-      HeartIcon,
-      CoffeeIcon
     },
     data () {
       return {
         html: '',
         showSetup: false,
         showSource: false,
+        showDonatePopup: false,
         version: require('../../package.json').version
-      };
+      }
     },
     computed: {
+      ...mapState(['app']),
       ...mapGetters({
         basic: 'getBasic',
         options: 'getOptions',
@@ -125,14 +201,14 @@
       }),
       selectTemplate: {
         get () {
-          return this.template.selected;
+          return this.template.selected
         },
         set (v) {
-          this.$store.dispatch('updateTemplate', v);
+          this.$store.dispatch('updateTemplate', v)
         }
       },
-      showSignature () {
-        return process.env.NODE_ENV === 'production';
+      isProd () {
+        return process.env.NODE_ENV === 'production'
       }
     },
     methods: {
@@ -143,106 +219,130 @@
         this.$refs.html.innerHTML = this.parseHTML()
         this.$refs.html.select()
         document.execCommand('copy')
-        this.gaEventClick('copy as HTML');
+        this.gaEventClick('copy as HTML')
       },
       copySelect () {
         if (window.getSelection) {
-          let range = document.createRange();
-          range.selectNode(this.$refs.preview.querySelector('.email-preview div'));
-          window.getSelection().removeAllRanges();
-          window.getSelection().addRange(range);
-          document.execCommand('copy');
-          this.gaEventClick('copy as select');
+          let range = document.createRange()
+          range.selectNode(this.$refs.preview.querySelector('.email-preview div'))
+          window.getSelection().removeAllRanges()
+          window.getSelection().addRange(range)
+          document.execCommand('copy')
+          this.gaEventClick('copy as select')
         }
       },
       viewSource () {
         this.html = this.parseHTML()
-        this.showSource = true;
-        this.gaEventClick('view source');
+        this.showSource = true
+        this.gaEventClick('view source')
       },
       copySource () {
-        this.$refs.dialogSource.select();
-        document.execCommand('copy');
-        this.gaEventClick('copy source');
+        this.$refs.dialogSource.select()
+        document.execCommand('copy')
+        this.gaEventClick('copy source')
       },
-      onDonate () {
-        this.gaEventClick('donate');
+      onClickHelp () {
+        this.gaEventClick('help open')
+      },
+      togglePromo (bool) {
+        this.$store.commit('SET_PROMOTE_SIGNATURE', bool)
+        if (bool) {
+          this.gaEventClick('remove promote signature')
+        } else {
+          this.gaEventClick('add promote signature')
+        }
       }
     }
-  };
+  }
 </script>
 
 <style lang="scss">
   @import "../assets/scss/variables.scss";
-
   .preview {
     background-color: #f6f6f6;
     padding: 40px 50px 20px 50px;
     overflow-y: auto;
     display: grid;
     grid-template-rows: 1fr 50px;
-
     &__header {
       margin-bottom: 20px;
     }
-
     &__inner {
       max-width: 900px;
     }
-
     &__footer {
       max-width: 900px;
       text-align: right;
       align-self: flex-end;
     }
-
     h2 {
       line-height: 0;
     }
-
     p {
       margin: 0;
     }
-
     .actions {
       text-align: right;
       margin-top: 20px;
     }
   }
-
   .email {
     min-height: 350px;
     width: 100%;
     background: #fff;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-
     &-content {
       padding: 20px;
-
+      p {
+        line-height: 1.5em;
+        margin-bottom: 10px;
+        font-size: 13px;
+      }
       .line {
         height: 10px;
         background-color: #eee;
         margin: 10px 0;
         border-radius: 2px;
-
         &.short {
           width: 30%;
         }
-
         &.long {
           width: 90%;
         }
-
+        &.half {
+          width: 50%;
+        }
         &.full {
           width: 100%;
         }
       }
+      .flex {
+        display: flex;
+        width: 100%;
+        margin-bottom: -10px;
+        .column-1 {
+          .line {
+            &:first-of-type {
+              margin-top: 0;
+            }
+          }
+        }
+        .column-1,
+        .column-2 {
+          flex-grow: 1;
+        }
+        .column-1 {
+          width: 100%;
+        }
+        .column-2 {
+          flex-shrink: 0;
+          min-width: 320px;
+        }
+      }
     }
-
     &-preview {
       padding: 20px;
     }
-
     .placeholder {
       height: 100px;
       width: 100px;
@@ -251,65 +351,68 @@
       font-size: 0;
     }
   }
-
   .setup-instruction {
     font-size: 12px;
   }
-
   .dialog-actions {
     text-align: right;
     margin-top: 20px;
   }
-
-  .copyright {
-    font-size: 11px;
+  .support-project {
     color: #aaa;
-
-    svg {
-      fill: #aaa;
-      width: 12px;
-      position: relative;
-      top: 2px;
-      padding: 0 4px 0 2px;
-    }
-
-    a {
-      color: inherit;
-      text-decoration: none;
-
+    font-size: 12px;
+    .support-shield {
+      display: inline-block;
+      padding: 12px 20px;
+      color: #444;
+      border-radius: 5px;
+      font-weight: bold;
+      border: 2px solid lighten(orange, 25%);
+      outline: none;
+      cursor: pointer;
+      transition: all .2s;
       &:hover {
-        color: #555;
+        border-color: orange;
       }
     }
-  }
-
-  .btn-donate {
-    display: inline-block;
-    text-decoration: none;
-    // padding: 5px 5px;
-    // background-color: orange;
-    color: orange;
-    line-height: 25px;
-
-    &:hover {
-      color: lighten(orange, 20%);
-
-      svg {
-        fill: lighten(orange, 20%);
+    a {
+      display: inline-block;
+      padding: 0 5px;
+      &:hover {
+        svg {
+          fill: $color-primary;
+        }
       }
     }
-
     svg {
+      width: 20px;
       position: relative;
-      top: 4px;
-      width: 24px;
-      fill: orange;
+      top: 5px;
+      transition: all 0.2s;
     }
   }
-
   .version {
     color: #aaa;
     margin-top: 10px;
     font-size: 12px;
+    span {
+      + span {
+        &::before {
+          content: "•";
+          padding: 0 5px;
+        }
+      }
+    }
+    svg {
+      width: 20px;
+      position: relative;
+      top: 5px;
+      transition: all 0.2s;
+    }
+  }
+  .link {
+    text-decoration: underline;
+    color: $color-primary;
+    cursor: pointer;
   }
 </style>
---
+++ src/components/templates/components/PromoteSignature.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <table
+    style="font-size: 0; margin-top: 10px;"
+    cellspacing="0"
+    cellpadding="0"
+    border="0"
+    role="presentation"
+  >
+    <tbody>
+    <tr>
+      <td>
+        <span style="font-size: 10px; color: #888; border-top: 1px solid #eee; padding-top: 5px;">Email signature by <a href="https://mechanikadesign.com/sigmark?ref=email">Sigmark</a></span>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'PromoteSignature',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
