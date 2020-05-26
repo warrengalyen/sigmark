25e62fe3143ffe3cefb77b10b05adeee67546a37
Warren Galyen <wgalyen@gmail.com>
2020-05-26 13:55:56 -0700

    chore: clean up

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="projects">
-    <!-- <h3>Current: {{ projects.project.name}}</h3> -->
     <el-table :data="allProjects" size="small" v-loading="loading" @cell-mouse-enter="onHover">
       <el-table-column label="Name" prop="name">
         <template slot-scope="scope">
@@ -29,10 +28,6 @@
               <span v-else>{{ scope.row.name }}</span>
             </span>
           </span>
-          <!-- <span v-if="projects.project.id === scope.row.id">
-            <strong>{{ scope.row.name }}</strong>
-          </span>
-          <span v-else>{{ scope.row.name }}</span>-->
         </template>
       </el-table-column>
       <el-table-column label="Actions" width="220">
